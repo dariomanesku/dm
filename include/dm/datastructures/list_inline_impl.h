ad61c49639f73195c9e26d3af316faec2b990f70
Dario Manesku <dariomanesku@gmail.com>
2015-02-10 23:12:33 +0100

    Rename.

--- include/dm/datastructures/list_inline_impl.h
+++ include/dm/datastructures/list_inline_impl.h
@@ -58,7 +58,7 @@ Ty* getObjFromHandle(uint16_t _handle)
 }
 
 private:
-Ty* getObjAtImpl(uint16_t _idx)
+Ty* getObjAt_impl(uint16_t _idx)
 {
     DM_CHECK(_idx < max(), "listGetObjAt | %d, %d", _idx, max());
 
@@ -70,25 +70,25 @@ public:
 Ty* getObjAt(uint16_t _idx)
 {
     This* list = const_cast<This*>(this);
-    return list->getObjAtImpl(_idx);
+    return list->getObjAt_impl(_idx);
 }
 
 const Ty* getObjAt(uint16_t _idx) const
 {
     This* list = const_cast<This*>(this);
-    return list->getObjAtImpl(_idx);
+    return list->getObjAt_impl(_idx);
 }
 
 Ty& operator[](uint16_t _idx)
 {
     This* list = const_cast<This*>(this);
-    return *list->getObjAtImpl(_idx);
+    return *list->getObjAt_impl(_idx);
 }
 
 const Ty& operator[](uint16_t _idx) const
 {
     This* list = const_cast<This*>(this);
-    return *list->getObjAtImpl(_idx);
+    return *list->getObjAt_impl(_idx);
 }
 
 void remove(uint16_t _handle)
