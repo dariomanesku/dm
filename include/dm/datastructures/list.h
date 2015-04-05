3e94217e8dce48dfaf94b688ef4004f8c5ce9385
Dario Manesku <dariomanesku@gmail.com>
2015-04-05 08:24:21 +0200

    All data structures now store their own allocator.

--- include/dm/datastructures/list.h
+++ include/dm/datastructures/list.h
@@ -7,11 +7,10 @@
 #define DM_LIST_H_HEADER_GUARD
 
 #include <stdint.h> // uint32_t
-#include <new> // placement-new
+#include <new>      // placement-new
 
-#include "allocator.h" // DM_ALLOC()/DM_FREE()
 #include "../common/common.h" // DM_INLINE
-#include "../check.h" // DM_CHECK
+#include "../check.h"         // DM_CHECK
 
 #include "handlealloc.h"
 
@@ -51,16 +50,17 @@ namespace dm
         // Uninitialized state, init() needs to be called !
         List()
         {
+            m_memoryBlock = NULL;
         }
 
-        List(uint16_t _max)
+        List(uint16_t _max, bx::ReallocatorI* _reallocator)
         {
-            init(_max);
+            init(_max, _reallocator);
         }
 
-        List(uint16_t _max, void* _mem)
+        List(uint16_t _max, void* _mem, bx::AllocatorI* _allocator)
         {
-            init(_max, _mem);
+            init(_max, _mem, _allocator);
         }
 
         ~List()
@@ -79,26 +79,33 @@ namespace dm
         }
 
         // Allocates memory internally.
-        void init(uint16_t _max)
+        void init(uint16_t _max, bx::ReallocatorI* _reallocator)
         {
-            m_memoryBlock = DM_ALLOC(sizeFor(_max));
+            m_memoryBlock = BX_ALLOC(_reallocator, sizeFor(_max));
+            m_reallocator = _reallocator;
             m_cleanup = true;
 
-            void* ptr = m_handles.init(_max, m_memoryBlock);
+            void* ptr = m_handles.init(_max, m_memoryBlock, (bx::AllocatorI*)_reallocator);
             m_elements = (Ty*)ptr;
         }
 
         // Uses externaly allocated memory.
-        void* init(uint16_t _max, void* _mem)
+        void* init(uint16_t _max, void* _mem, bx::AllocatorI* _allocator)
         {
             m_memoryBlock = _mem;
+            m_allocator = _allocator;
             m_cleanup = false;
 
-            void* ptr = m_handles.init(_max, m_memoryBlock);
+            void* ptr = m_handles.init(_max, m_memoryBlock, _allocator);
             m_elements = (Ty*)ptr;
 
             void* end = (void*)((uint8_t*)_mem + sizeFor(_max));
             return end;
+        }
+
+        bool isInitialized() const
+        {
+            return (NULL != m_memoryBlock);
         }
 
         void destroy()
@@ -108,7 +115,7 @@ namespace dm
                 m_handles.destroy();
                 if (m_cleanup)
                 {
-                    DM_FREE(m_memoryBlock);
+                    BX_FREE(m_reallocator, m_memoryBlock);
                 }
                 m_memoryBlock = NULL;
             }
@@ -126,31 +133,41 @@ namespace dm
             return m_handles.max();
         }
 
+        bx::AllocatorI* allocator()
+        {
+            return m_allocator;
+        }
+
     private:
         HandleAlloc m_handles;
         Ty* m_elements;
         void* m_memoryBlock;
+        union
+        {
+            bx::AllocatorI*   m_allocator;
+            bx::ReallocatorI* m_reallocator;
+        };
         bool m_cleanup;
     };
 
     template <typename Ty/*obj type*/>
-    DM_INLINE List<Ty>* createList(uint16_t _max, void* _mem)
+    DM_INLINE List<Ty>* createList(uint16_t _max, void* _mem, bx::AllocatorI* _allocator)
     {
-        return ::new (_mem) List<Ty>(_max, (uint8_t*)_mem + sizeof(List<Ty>));
+        return ::new (_mem) List<Ty>(_max, (uint8_t*)_mem + sizeof(List<Ty>), _allocator);
     }
 
     template <typename Ty/*obj type*/>
-    DM_INLINE List<Ty>* createList(uint16_t _max)
+    DM_INLINE List<Ty>* createList(uint16_t _max, bx::AllocatorI* _allocator)
     {
-        uint8_t* ptr = (uint8_t*)DM_ALLOC(sizeof(List<Ty>) + List<Ty>::sizeFor(_max));
-        return createList<Ty>(_max, ptr);
+        uint8_t* ptr = (uint8_t*)BX_ALLOC(_allocator, sizeof(List<Ty>) + List<Ty>::sizeFor(_max));
+        return createList<Ty>(_max, ptr, _allocator);
     }
 
     template <typename Ty/*obj type*/>
     DM_INLINE void destroyList(List<Ty>* _list)
     {
         _list->~List<Ty>();
-        delete _list;
+        BX_FREE(_list->allocator(), _list);
     }
 
 } // namespace dm
