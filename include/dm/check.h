7e8d4bdc325331b0a8af7ee5f03a3687f1e7754c
Dario Manesku <dariomanesku@gmail.com>
2016-08-21 19:00:59 +0200

    Added DM_DEBUG_EVAL() and some cleanup.

--- include/dm/check.h
+++ include/dm/check.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2015 Dario Manesku. All rights reserved.
+ * Copyright 2014-2016 Dario Manesku. All rights reserved.
  * License: http://www.opensource.org/licenses/BSD-2-Clause
  */
 
@@ -25,46 +25,56 @@
 // Implementation.
 //-----
 
-#define DM_STRINGIZE(_x) DM_STRINGIZE_(_x)
-#define DM_STRINGIZE_(_x) #_x
-#define DM_FILE_LINE "" __FILE__ "(" DM_STRINGIZE(__LINE__) ")"
+#define DM_CHECK_CONCATENATE(_x, _y) DM_CHECK_CONCATENATE_(_x, _y)
+#define DM_CHECK_CONCATENATE_(_x, _y) _x ## _y
 
-#define _DM_CHECK_NOOP(_condition, _format, ...) for (;;) { break; }
-#define _DM_CHECK_PRINT(_condition, _format, ...)                                          \
-    do                                                                                     \
-    {                                                                                      \
-        if (!(_condition))                                                                 \
-        {                                                                                  \
-            fprintf(stderr, "DM ERROR [" DM_FILE_LINE "]: "  _format "\n", ##__VA_ARGS__); \
-        }                                                                                  \
+#define DM_CHECK_FILE_LINE "" __FILE__ "(" DM_STRINGIZE(__LINE__) ")"
+
+#define _DM_CHECK_NOOP_IMPL(_condition, _format, ...) for (;;) { break; }
+#define _DM_CHECK_PRINT_IMPL(_condition, _format, ...) \
+    do                                                 \
+    {                                                  \
+        if (!(_condition))                             \
+        {                                              \
+            fprintf(stderr, "DM ERROR [" DM_CHECK_FILE_LINE "]: "  _format "\n", ##__VA_ARGS__); \
+        }                                              \
     } while(0)
-#define _DM_CHECK_BREAK(_condition, _format, ...)                                          \
-    do                                                                                     \
-    {                                                                                      \
-        if (!(_condition))                                                                 \
-        {                                                                                  \
-            fprintf(stderr, "DM ERROR [" DM_FILE_LINE "]: "  _format "\n", ##__VA_ARGS__); \
-            bx::debugBreak();                                                              \
-        }                                                                                  \
+#define _DM_CHECK_BREAK_IMPL(_condition, _format, ...) \
+    do                                                 \
+    {                                                  \
+        if (!(_condition))                             \
+        {                                              \
+            fprintf(stderr, "DM ERROR [" DM_CHECK_FILE_LINE "]: "  _format "\n", ##__VA_ARGS__); \
+            dm::ng::debugBreak();                      \
+        }                                              \
     } while(0)
 
-#if (DM_CHECK_CONFIG == DM_CHECK_CONFIG_PRINT)
-#    define _DM_CHECK _DM_CHECK_PRINT
-#    include <stdio.h> // fprintf()
-#elif (DM_CHECK_CONFIG == DM_CHECK_CONFIG_DEBUG_BREAK)
-#    define _DM_CHECK _DM_CHECK_BREAK
-#    include <stdio.h> // fprintf()
-#    include "../../3rdparty/bx/debug.h" // bx::debugBreak()
-#else
-#    define _DM_CHECK _DM_CHECK_NOOP
-#endif //(DM_CHECK_CONFIG == DM_CHECK_CONFIG_PRINT)
+// Setup.
+//-----
+
+#if !defined(DM_DEBUG_EVAL)
+#   if DM_DEBUG_BUILD
+#       define DM_DEBUG_EVAL(_expr) _expr
+#   else
+#       define DM_DEBUG_EVAL(_expr)
+#   endif //DM_DEBUG_BUILD
+#endif // !defined(DM_DEBUG_EVAL)
 
 #if !defined(DM_CHECK)
-#    define DM_CHECK _DM_CHECK
+#   if (DM_CHECK_CONFIG == DM_CHECK_CONFIG_PRINT)
+#       define DM_CHECK _DM_CHECK_PRINT_IMPL
+#       include <stdio.h> // fprintf()
+#   elif (DM_CHECK_CONFIG == DM_CHECK_CONFIG_DEBUG_BREAK)
+#       define DM_CHECK _DM_CHECK_BREAK_IMPL
+#       include <stdio.h> // fprintf()
+#       include "ng/debug.h" // dm::ng::debugBreak()
+#   else
+#       define DM_CHECK _DM_CHECK_NOOP_IMPL
+#   endif //(DM_CHECK_CONFIG == DM_CHECK_CONFIG_PRINT)
 #endif //!defined(DM_CHECK)
 
 #if !defined(DM_ASSERT)
-#    define DM_ASSERT(_condition) _DM_CHECK(_condition, "Assertion failed!")
+#    define DM_ASSERT(_condition) DM_CHECK(_condition, "Assertion failed!")
 #endif //!defined(DM_ASSERT)
 
 #endif // DM_CHECK_H_HEADER_GUARD
